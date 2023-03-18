85d2e84738d9450664f0c25feb0946c9d558ad41
Begopa <philip0813@naver.com>
2023-03-18 23:35:55 +0900

    typeORM 연결 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as process from 'process';
 
 @Module({
-  imports: [EmployeesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      port: 3306,
+      database: 'hr',
+      entities: [],
+      synchronize: true,
+    }),
+    EmployeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
