66257943226cdf6c8a1296216ed757c8671a1468
Begopa <philip0813@naver.com>
2023-03-18 16:53:34 +0900

    구현 로직 정리

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Patch } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 
-@Controller('employees')
+@Controller('public')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
+
+  @Get('employees:employee_id')
+  getEmployee() {
+    return '특정 사원 현재 정보';
+  }
+
+  @Get('employees/jobHistory/:employee_id')
+  getJobHistoryOfEmployee() {
+    return '특정 사원 이력';
+  }
+
+  @Get('department/:department_id')
+  getDepartment() {
+    return '부서 및 위치 정보';
+  }
+
+  @Patch('department/:department_id')
+  updateDepartmentSalary() {
+    return '특정부서 급여를 특정 비율로 인상 및 사원 정보 업데이트';
+  }
 }
